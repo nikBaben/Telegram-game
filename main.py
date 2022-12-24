ecf28adc1fab03d6d5f0b628db3f73575eab49f7
nikBaben <faymen44@mail.ru>
2022-12-24 23:16:05 +0300

    start working

---
+++ main.py
@@ -0,0 +1,23 @@
+from aiogram import Bot, Dispatcher, executor, types
+from data import create_db
+
+API_TOKEN = '5801245073:AAE8sJPIIVhz88j1tV-WRDQEG6ZV27x_mCM'
+
+
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher(bot)
+
+
+async def start(_): 
+    await create_db()
+
+@dp.message_handler(commands=['start'])
+async def send_welcome(message: types.Message):
+    await message.answer("Привет.")
+    
+
+
+
+
+if __name__ == "__main__":
+     executor.start_polling(dp, skip_updates=True,on_startup=start)
