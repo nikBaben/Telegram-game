984a9a7245b80f7f9157b75dd666effce4a72dc1
nikBaben <faymen44@mail.ru>
2022-12-25 02:32:48 +0300

    add counter

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from aiogram import Bot, Dispatcher, executor, types
-from data import create_db,create_profile
+from data import create_db,create_profile,get_user,get_balance,up_balance
+from datetime import datetime
+from threading import Timer
+import asyncio
+import time
+from aio_timers import Timer
 
-API_TOKEN = '5801245073:AAE8sJPIIVhz88j1tV-WRDQEG6ZV27x_mCM'
+API_TOKEN = '5978814839:AAHYphqvVnPtFq2bIr2NfqRbJ9xfYC8U65g'
 
 
 bot = Bot(token=API_TOKEN)
@@ -13,13 +18,29 @@ async def start(_):
 
 @dp.message_handler(commands=['start'])
 async def send_welcome(message: types.Message):
-    await message.answer("Привет")
     user_id = message.from_user.id
-    await create_profile(user_id = user_id)
+    user_name = message.from_user.first_name
+    print(user_name)
+    await create_profile(user_id = user_id,user_name = user_name)
+    a = await get_user()
+    await message.answer(f"Привет {a[0][1]} ")
 
 
 
 
+@dp.message_handler(commands=['start_game'])
+async def send_welcome(message: types.Message):
+    a = await get_user()
+    async def Task():
+      while True:
+        a = await get_user()
+        b = a[0][4]
+        await asyncio.sleep(2)
+        b +=1 
+        await up_balance(money = b,user_id = message.from_user.id)
+        print(b)
+    await message.answer(f"ВАШ БАЛАНС {a[0][4]}")
+    await Task()
 
 if __name__ == "__main__":
      executor.start_polling(dp, skip_updates=True,on_startup=start)
