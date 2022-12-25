b8c1aff2d0fe83d28a7d88a049a27b61984d5ecd
nikBaben <faymen44@mail.ru>
2022-12-25 14:43:11 +0300

    add multiprocessing

--- data.py
+++ data.py
@@ -6,7 +6,7 @@ async def create_db():
     global cur,db
     db = sqlite3.connect("data.db")
     cur = db.cursor()
-    cur.execute("CREATE TABLE IF NOT EXISTS data_game (login STR, name STR, level int default 1, updrade int default 1, balance  int default 0)")
+    cur.execute("CREATE TABLE IF NOT EXISTS data_game (login STR, name STR, level int default 1, updrade int default 1, balance  int default 0,game_start  int default 0)")
     db.commit()
 
 
@@ -15,7 +15,14 @@ async def create_profile(user_id,user_name):
     cur.execute("INSERT INTO data_game (login,name) VALUES (?,?)", user )
     db.commit()
 
-
+async def get_start(user_id): 
+    a = cur.execute("SELECT game_start FROM data_game WHERE login=?", (user_id,)).fetchone()
+    return a
+async def update_start(flag,user_id): 
+    user = (flag,user_id)
+    a = cur.execute("UPDATE data_game set game_start = ? WHERE login = ?", user)
+    db.commit()
+    return a
 
 async def get_balance(user_id):
     user = user_id
@@ -26,7 +33,8 @@ async def get_balance(user_id):
 
 async def up_balance(money,user_id):
     user = (money,user_id)
-    a = cur.execute("UPDATE data_game set balance = ? WHERE login = ? ", user)
+    a = cur.execute("UPDATE data_game set balance = ? WHERE login = ?", user)
+    db.commit()
     return a
 
 async def get_user(user_id): 
--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from aiogram import Bot, Dispatcher, executor, types
-from data import create_db,create_profile,get_user,get_balance,up_balance
+from data import create_db,create_profile,get_user,get_balance,up_balance,get_start,update_start
 from datetime import datetime
 from threading import Timer
 import asyncio
 import time
 from aio_timers import Timer
+from threading import *
+from multiprocessing import Process
+
 
 API_TOKEN = '5978814839:AAHYphqvVnPtFq2bIr2NfqRbJ9xfYC8U65g'
 
@@ -26,9 +29,7 @@ async def send_welcome(message: types.Message):
     await message.answer(f"Привет {a[0]} ")
 
 
-
-
-@dp.message_handler(commands=['l'])
+@dp.message_handler(commands=['start_gmae'])
 async def send_welcome(message: types.Message):
     a = await get_balance(message.from_user.id)
     async def Task():
@@ -39,10 +40,24 @@ async def send_welcome(message: types.Message):
         b +=1 
         await up_balance(money = b,user_id = message.from_user.id)
         print(b)
-    await message.answer(f"СОСИ {a[0]}")
-     
-    await Task()
+        print(await get_start(user_id = message.from_user.id))
+    start = await get_start(user_id = message.from_user.id)
+    if start[0]==0: 
+        await update_start(flag = 1,user_id= message.from_user.id)
+        await message.answer("Игра Началась, каждую секунду вам будут начисляться монеты")
+        await Task()
+    if start[0] == 1: 
+        await message.answer("вы не можете начать игру 2 раза!")
     
 
+@dp.message_handler(commands=['balance'])
+async def send_welcome(message: types.Message):
+    a = await get_balance(user_id = message.from_user.id)
+    await message.answer(f"ВАШ БАЛАНС - {a[0]}")
+
+
 if __name__ == "__main__":
-     executor.start_polling(dp, skip_updates=True,on_startup=start)
+    executor.start_polling(dp, skip_updates=True,on_startup=start)
+    t1 = Process(target = send_welcome)
+    t1.start()
+    t1.join()
